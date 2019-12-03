834ffb1f9bdb80338341ea1e5a74876b190c33bf
azul915 <k.aoyagi940516@gmail.com>
2019-12-04 04:09:16 +0900

    firebaseSDKをインストール

--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module app
 go 1.13
 
 require (
+	cloud.google.com/go/firestore v1.1.0 // indirect
+	cloud.google.com/go/storage v1.4.0 // indirect
+	firebase.google.com/go v3.10.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	google.golang.org/api v0.14.0
+	google.golang.org/appengine v1.6.5 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import (
     "net/http"
+    "fmt"
 
     "github.com/labstack/echo"
+
+    "golang.org/x/net/context"
+    firebase "firebase.google.com/go"
+
+    "google.golang.org/api/option"
 )
 
+func initializeAppWithServiceAccount() *firebase.App {
+
+    opt := option.WithCredentialsFile("./portfolio-firebase-adminsdk.json")
+    app, err := firebase.NewApp(context.Background(), nil, opt)
+    if err != nil {
+        log.Fatalf("error initializing app: %v\n", err)
+    }
+
+    return app
+}
+
 func main() {
+
+    app := initializeAppWithServiceAccount()
+    fmt.Print(app)
+    if err != nil {
+        panic(err)
+    }
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "hoge!")
