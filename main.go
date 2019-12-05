8fba1cf81f8ed0cd7ebbd9c10547bbe9bfff94c2
azul915 <k.aoyagi940516@gmail.com>
2019-12-06 08:30:05 +0900

    PathParameterを受け取る

--- main.go
+++ main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
+
+    _ "fmt"
+    _"context"
     "net/http"
-    "fmt"
 
     "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 
-    "golang.org/x/net/context"
-    firebase "firebase.google.com/go"
+    _ "cloud.google.com/go/firestore"
+    _ "firebase.google.com/go"
+    _ "google.golang.org/api/option"
 
-    "google.golang.org/api/option"
 )
-
-func initializeAppWithServiceAccount() *firebase.App {
-
-    opt := option.WithCredentialsFile("./portfolio-firebase-adminsdk.json")
-    app, err := firebase.NewApp(context.Background(), nil, opt)
-    if err != nil {
-        log.Fatalf("error initializing app: %v\n", err)
-    }
-
-    return app
-}
 
 func main() {
 
-    app := initializeAppWithServiceAccount()
-    fmt.Print(app)
-    if err != nil {
-        panic(err)
-    }
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "hoge!")
-    })
-    e.Logger.Fatal(e.Start(":1999"))
+
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
+    e.GET("/hello/:username", MainPage())
+
+    e.Start(":1999")
+
+}
+
+func MainPage() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        username := c.Param("username")
+        return c.String(http.StatusOK, "Hello World " + username)
+    }
 }
