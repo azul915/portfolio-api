f235771c36163272f7e861f39d4b60805ee98d4b
azul915 <k.aoyagi940516@gmail.com>
2019-12-09 23:12:05 +0900

    fix format main.go

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-
     "context"
     "encoding/json"
-    "fmt"
     "log"
     "net/http"
     "time"
@@ -13,7 +11,6 @@ import (
     "firebase.google.com/go"
     "google.golang.org/api/option"
 )
-
 
 func firebaseInit(ctx context.Context) (*firestore.Client, error) {
 
@@ -49,20 +46,19 @@ func getSkills(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(skills)
 }
 
-
 func addSkill(w http.ResponseWriter, r *http.Request) {
 
-    s := Skill {
-        Category: Category {
-            ID: 2,
+    s := Skill{
+        Category: Category{
+            ID:   2,
             Name: "フレームワーク",
         },
         CreatedAt: time.Now(),
-        Detail: "軽量なAPIを実装したことがある。",
-        Duration: 2,
-        Name: "Echo",
-        SelfEval: 1,
-        Term: "serverside",
+        Detail:    "軽量なAPIを実装したことがある。",
+        Duration:  2,
+        Name:      "Echo",
+        SelfEval:  1,
+        Term:      "serverside",
     }
     err := setSkill(s)
     if err != nil {
@@ -72,37 +68,30 @@ func addSkill(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(err)
 }
 
+func deleteSkill(w http.ResponseWriter, r *http.Request) {
 
-// func deleteSkill(c echo.Context) error {
+    req := delSkill{
+        Name: "PHP",
+        Term: "serverside",
+    }
 
-//     req := delSkill {
-//         Name: "Flask",
-//         Term: "serverside",
-//     }
+    err := delete(req)
+    if err != nil {
+        log.Fatalln(err)
+    }
 
-//     err := delete(req)
-//     if err != nil {
-//         log.Fatalln(err)
-//     }
-
-//     return c.JSON(http.StatusOK, err)
-// }
+    json.NewEncoder(w).Encode(err)
+}
 
 func handleRequests() {
     http.HandleFunc("/", getSkills)
     http.HandleFunc("/add", addSkill)
+    http.HandleFunc("/delete", deleteSkill)
     log.Fatal(http.ListenAndServe(":1999", nil))
 }
 
-
 func main() {
 
-    // e.GET("/delete", deleteSkill)
     handleRequests()
 
-}
-
-
-func p(a ...interface{}) {
-    fmt.Println(a...)
 }
