4997cb9634e41a1245c6f73341ad5bf15b81c607
azul915 <k.aoyagi940516@gmail.com>
2020-02-05 02:24:49 +0900

    Add metaInfo skill struct

--- src/domain/skill.go
+++ src/domain/skill.go
@@ -5,21 +5,21 @@ import "time"
 type Skills []Skill
 
 type Skill struct {
-	Category  Category  `firestore:"category" binding:"required"`
-	CreatedAt time.Time `firestore:"created_at" binding:"required"`
-	Detail    string    `firestore:"detail" binding:"required"`
-	Duration  int64     `firestore:"duration" binding:"required"`
-	Name      string    `firestore:"name" binding:"required"`
-	SelfEval  int64     `firestore:"self_evaluation" binding:"required"`
-	Term      string    `firestore:"term" binding:"required"`
+	Category  Category  `form:"category" json:"category" firestore:"category" binding:"required"`
+	CreatedAt time.Time `form:"created_at" json:"created_at" firestore:"created_at" binding:"required"`
+	Detail    string    `form:"detail" json:"detail" firestore:"detail" binding:"required"`
+	Duration  int64     `form:"duration" json:"duration" firestore:"duration" binding:"required"`
+	Name      string    `form:"name" json:"name" firestore:"name" binding:"required"`
+	SelfEval  int64     `form:"self_evaluation" json:"self_evaluation" firestore:"self_evaluation" binding:"required"`
+	Term      string    `form:"term" json:"term" firestore:"term" firestore:"term" binding:"required"`
 }
 
 type Category struct {
-	ID   int64  `firestore:"id" binding:"required"`
-	Name string `firestore:"name" binding:"required"`
+	ID   int64  `form: "id" json:"id" firestore:"id" binding:"required"`
+	Name string `form: "name" json:"name" firestore:"name" binding:"required"`
 }
 
 type DelSkill struct {
-	Name      string     `firestore:"name" binding:"required"`
-	Term      string     `firestore:"term" binding:"required"`
+	Name      string     `form:"name" json:"name" firestore:"name" binding:"required"`
+	Term      string     `form:"term" json:"term" firestore:"term" binding:"required"`
 }
